a74f1322915cc0bed7bdd975372694b1227edd2d
Aabha Tomar <tomaraabha2@gmail.com>
2026-04-01 15:38:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,25 +13,33 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    transactions = df.head(15).to_dict(orient="records")
+    df = pd.read_excel("data.xlsx")
+
+    transactions = df.to_dict(orient="records")
+
     total_txn = len(df)
     suspicious_txn = len(df[df["label"] == "Suspicious"])
     normal_txn = len(df[df["label"] == "Normal"])
 
-    # Hour-wise transactions
-    hour_data = df["hour"].value_counts().sort_index()
+    hours = df["hour"].tolist()
+    counts = df["txn_count_1hr"].tolist()
 
-    hours = list(map(int, hour_data.index))
-    counts = list(map(int, hour_data.values))
+    # 👇 YE NAYA PART
+    amounts = list(map(int, df["amount"]))
+    txn_counts = list(map(int, df["txn_count_1hr"]))
+    labels = df["label"].tolist()
 
     return render_template(
         "dashboard.html",
         transactions=transactions,
-        total_txn=total_txn,
-        suspicious_txn=suspicious_txn,
-        normal_txn=normal_txn,
+        total_txn=int(total_txn),
+        suspicious_txn=int(suspicious_txn),
+        normal_txn=int(normal_txn),
         hours=hours,
-        counts=counts
+        counts=counts,
+        amounts=amounts,
+        txn_counts=txn_counts,
+        labels=labels
     )
 
 if __name__ == "__main__":
